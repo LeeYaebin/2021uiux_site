0734a6c6c903fecac14822c82a969ac6c0a7883b
LeeYaebin <qls2920@naver.com>
2021-05-12 14:54:05 +0900

    .

--- 04_company/js0/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $(".slider").bxSlider({
-    pager:false,
-    auto:true
-  });
-  $(".suv_menu ul li").click(function(){
-    $(this).addClass("on").siblings().removeClass("on");
-  });
-});
