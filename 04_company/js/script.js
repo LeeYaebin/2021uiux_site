ec54721edd618cdbe56a50733b3e8601707f8d9b
LeeYaebin <qls2920@naver.com>
2021-05-12 18:23:21 +0900

    .

--- 04_company/js/script.js
+++ 04_company/js/script.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
   $(".slider").bxSlider({
     pager:false,
-    auto:true
+    auto:true,
+    controls:false
   });
   $(".suv_menu ul li").click(function(){
     $(this).addClass("on").siblings().removeClass("on");
+  });
+
+  $(".toggleMenu").click(function(){
+    const toggle = $(".toggleMenu").attr("class");
+    if(toggle == "toggleMenu"){
+    $(".toggleMenu").addClass("change");
+    //$("#myTopMenu").css("left",0);
+    $("#main_menu").stop().animate({left:0});
+
+  }else{
+    $(".toggleMenu").removeClass("change");
+    //$("#myTopMenu").css("left","-100%");
+    $("#main_menu").stop().animate({left:"-100%"});
+  }
+  });
+
+  $(window).resize(function(){
+    $("#main_menu").removeAttr("style");
+    $(".toggleMenu").removeClass("change");
   });
 });
